3b73fdcea2c085c5411d60d390a39d53d6af3171
rumsha-patil <rumsha.cmc@gmail.com>
2019-10-13 20:57:07 +0530

    Added Query parameters

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -7,7 +7,8 @@ module.exports = {
     AddCar: async (req, res) => {
         try {
             const {number, model,capacity,cost_per_day,_id } = req.body;
-            if(await CarModel.findOne({ number: number })){
+            let num =  number.replace(/ /g,'').toLowerCase()
+            if(await CarModel.findOne({ number: num})){
                 return res.json({
                     error: {
                         error: true,
@@ -18,15 +19,13 @@ module.exports = {
 
             //Create new car 
             const car = new CarModel({
-                number: number.replace(/ /g,'').toLowerCase(),
+                number: num,
                 model: model,
                 capacity: capacity,
                 cost_per_day: cost_per_day,
                 added_by: mongoose.Types.ObjectId(_id),
                 reserved: []
             });
-
-
             car.save(function(err,newUser) {
                 if (err) throw Error(`Error occurred while saving ${err}`);
                 return res.status(200).json({
@@ -49,17 +48,38 @@ module.exports = {
     },
     GetCars: async (req, res) => {
         try {
-            CarModel.find({
-                model: new RegExp('^'+req.body.model+'$', "i"),
-                capacity: {$gte: req.body.capacity},
-                cost_per_day: {$gte: req.body.priceRange.lower, $lte: req.body.priceRange.upper},
-                reserved: { 
-                    $not: {
-                        $elemMatch: {from: {$lt: req.body.to.substring(0,10)}, to: {$gt: req.body.from.substring(0,10)}}
-                    }
-    
+            const {model,capacity,priceRange,_id,from,to} = req.body;
+
+            query = {};
+            if(model){
+                query["model"] =  new RegExp('^'+model+'$', "i");
+            }
+            if(capacity){
+                query["capacity"] = {};
+                query["capacity"]["$gte"] = capacity;
+            }
+            if(priceRange){
+                query["cost_per_day"] = {}
+                if(priceRange.lower){
+                    query["cost_per_day"]["$gte"] = priceRange.lower;
                 }
-            },function(error, cars){
+                if(priceRange.upper){
+                    query["cost_per_day"]["$lte"] = priceRange.upper;
+                }
+            }
+            //Date validation
+            if(to || from){ 
+                query["reserved"] = {};
+                query["reserved"]["$not"] = {};
+                query["reserved"]["$not"]["$elemMatch"] = {};
+                if(to){
+                    query["reserved"]["$not"]["$elemMatch"]["from"] = {$lt: new Date(to)};
+                }
+                if(from){
+                    query["reserved"]["$not"]["$elemMatch"]["to"] = {$gt: new Date(to)};
+                }
+            }
+            CarModel.find(query,function(error, cars){
                 if (error) throw Error(`Error occurred ${error}`);
                 return res.status(200).json({
                     error: {
