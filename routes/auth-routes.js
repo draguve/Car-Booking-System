ef10f8d874c3bc4ec21938839a8eb83d327dbc64
rumsha-patil <rumsha.cmc@gmail.com>
2019-10-14 19:42:59 +0530

    Auth routes

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -4,8 +4,8 @@ const { Register, Login } = require("../controllers/authController");
 let router = require('express').Router();
 // Set default API response
 
-router.post("/auth/register", Register);
-router.post("/auth/login", Login);
+router.post("/register", Register);
+router.post("/login", Login);
 
 // Export API routes
 module.exports = router;
